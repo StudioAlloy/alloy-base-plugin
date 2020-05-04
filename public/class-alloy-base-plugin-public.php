9466759e8efae0f7a8900e5c64e4377937b21f8d
Mitchel van Eijgen <mvaneijgen@gmail.com>
2020-05-04 13:13:39 +0200

    Removed front-end scripts

--- public/class-alloy-base-plugin-public.php
+++ public/class-alloy-base-plugin-public.php
@@ -59,45 +59,45 @@ class Alloy_Base_Plugin_Public {
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles() {
+// 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Alloy_Base_Plugin_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Alloy_Base_Plugin_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+// 		/**
+// 		 * This function is provided for demonstration purposes only.
+// 		 *
+// 		 * An instance of this class should be passed to the run() function
+// 		 * defined in Alloy_Base_Plugin_Loader as all of the hooks are defined
+// 		 * in that particular class.
+// 		 *
+// 		 * The Alloy_Base_Plugin_Loader will then create the relationship
+// 		 * between the defined hooks and the functions defined in this
+// 		 * class.
+// 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/alloy-base-plugin-public.css', array(), $this->version, 'all' );
+// 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/alloy-base-plugin-public.css', array(), $this->version, 'all' );
 
-	}
+// 	}
 
 	/**
 	 * Register the JavaScript for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_scripts() {
+// 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Alloy_Base_Plugin_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Alloy_Base_Plugin_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+// 		/**
+// 		 * This function is provided for demonstration purposes only.
+// 		 *
+// 		 * An instance of this class should be passed to the run() function
+// 		 * defined in Alloy_Base_Plugin_Loader as all of the hooks are defined
+// 		 * in that particular class.
+// 		 *
+// 		 * The Alloy_Base_Plugin_Loader will then create the relationship
+// 		 * between the defined hooks and the functions defined in this
+// 		 * class.
+// 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/alloy-base-plugin-public.js', array( 'jquery' ), $this->version, false );
+// 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/alloy-base-plugin-public.js', array( 'jquery' ), $this->version, false );
 
-	}
+// 	}
 
 }
